4470f7287cbfc0b14b61123bcbcbcb9d0242b22c
lblk <dug.lblk@gmail.com>
2022-09-28 21:13:54 +0800

    修复正版

--- typeDump.h
+++ typeDump.h
@@ -1,3 +1,13 @@
+#if 0
 template <typename>
 struct [[deprecated]]typeDump{};
-
+#else
+//for gcc
+template <typename, typename...>
+struct [[gnu::warning("your type here")]] typeDump {};
+#endif
+/*
+ * following for clang
+template <typename, typename...>
+struct [[deprecated]] dump{};
+*/
